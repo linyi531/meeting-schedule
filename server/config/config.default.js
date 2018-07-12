ecb0bdd6b3b36370eb00f5c9efcc4cf656a2276f
lilinyi <LOVER12161@163.com>
2018-07-12 11:10:01 +0800

    init next branch

--- server/config/config.default.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const R = require('ramda')
-
-const config = {
-  oneDayMs: 86400000 // 一天的毫秒数
-}
-
-module.exports = R.merge(config, process.env.NODE_ENV === 'development' ? require('./config.dev') : require('./config.prod'))
--- watchDog/README.md
+++
@@ -1,5 +0,0 @@
-# react_mobx
-
-npm install
-
-npm run dev
